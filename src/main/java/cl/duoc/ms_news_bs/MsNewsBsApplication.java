4d08c222b414eb3aa14d1de92112afb6c52edfb0
ibliyhsb <catd.rojas@duocuc.cl>
2025-12-10 02:24:47 -0300

    proyecto base

---
+++ src/main/java/cl/duoc/ms_news_bs/MsNewsBsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_news_bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNewsBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNewsBsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_news_bs/MsNewsBsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_news_bs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNewsBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
