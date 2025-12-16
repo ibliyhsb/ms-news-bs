4bc30f77300be1adce177993bdfeb2e47890969c
Alemstar <al.faguas@duocuc.cl>
2025-12-15 22:42:07 -0300

    modificaciones en microservicio de noticias bs

--- src/main/java/cl/duoc/ms_news_bs/model/dto/NewsArticleDto.java
+++ src/main/java/cl/duoc/ms_news_bs/model/dto/NewsArticleDto.java
@@ -1,5 +1,6 @@
 package cl.duoc.ms_news_bs.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewsArticleDto {
+    @JsonProperty("id")
     private Long id;
-    private String title;
-    private String source;
-    private String content;
-    private String url;
+    
+    @JsonProperty("titulo")
+    private String titulo;
+    
+    @JsonProperty("descripcion")
+    private String descripcion;
+    
+    @JsonProperty("contenido")
+    private String contenido;
+    
+    @JsonProperty("autor")
+    private String autor;
+    
+    @JsonProperty("fechaPublicacion")
+    private String fechaPublicacion;
+    
+    @JsonProperty("esPublicado")
+    private Boolean esPublicado;
+    
+    @JsonProperty("imagenUri")
+    private String imagenUri;
 }
